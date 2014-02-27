1186a312e72467c317e48f701af52c75bb168789
Yzerman <UserPass25>
2014-02-27 12:45:13 +0100

    l2ha_1

---
+++ ConsoleApplication2/ConsoleApplication2/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace L2HA1_Keusch_Patrice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string time = DateTime.Now.ToShortTimeString();
+            Console.WriteLine(time);
+            Console.Read();
+        }
+    }
+}
