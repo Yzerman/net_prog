063529c9aec6540294b4211d666d2219bca4358c
Yzerman <UserPass25>
2014-03-05 15:41:57 +0100

    L3Ha1

---
+++ L3HA1_Patrice_Keusch/IEmployee.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Employees
+{
+    public interface IEmployee
+    {
+        int Age
+        {
+            get;
+            set;
+        }
+
+        double benefit
+        {
+            get;
+            set;
+        }
+
+        string Company
+        {
+            get;
+            set;
+        }
+
+        int ID
+        {
+            get;
+            set;
+        }
+
+        /// <summary>
+        /// </summary>
+        string Name
+        {
+            get;
+            set;
+        }
+
+        int Pay
+        {
+            get;
+            set;
+        }
+
+        int SocialSecurityNumber
+        {
+            get;
+            set;
+        }
+
+        void DisplayStats();
+
+        double GetBenefitCost();
+
+        void GiveBonus();
+    }
+}
---
+++ L3HA1_Patrice_Keusch/ItSpecialist.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Employees
+{
+    class ItSpecialist : Employee
+    {
+        public bool MicrosoftSpezi {get;set;}
+   
+        public bool LinuxSpezi {get;set;}
+     
+        public void InstallServer()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void ConfigureApp()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public override void DisplayStats()
+        {
+            Console.WriteLine("Ist Microsoft Spezi: " + MicrosoftSpezi);
+            Console.WriteLine("Ist Linux Spezi: " + LinuxSpezi);
+        }
+    }
+}
