8b75a06778d6c419108e70f4f58bf4d38a631ed3
Yzerman <yzerman.detroit@bluewin.ch>
2014-03-11 19:38:17 +0100

    L4M1

---
+++ L4M1/L4M1/IZhawCal.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace L4M1
+{
+    public interface IZhawCal
+    {
+        double Add(double e1, double e2);
+        double Sub(double e1, double e2);
+        double Multi(double e1, double e2);
+        double Div(double e1, double e2);
+
+    }
+}
---
+++ L4M1/L4M1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace L4M1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Excpetion Test");
+            new Math().Div(1, 0);
+        }
+    }
+}
