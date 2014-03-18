3ee2cf85af4cf2470464bbdf5da5cce575e029bf
Yzerman <UserPass25>
2014-03-18 11:15:31 +0100

    l

---
+++ abgabe/L2HA_Keusch_Patrice/Aufgabe2/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace L2HA2_Keusch_Patrice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double test = 1;
+
+            Console.WriteLine(Converter.inchTocm(test));
+            Console.ReadLine();
+            double test1 = 2.54;
+            Console.WriteLine(Converter.cmToinch(test1));
+            Console.ReadLine();
+        }
+    }
+
+    class  Converter
+    {
+
+        public static double inchTocm(double inch)
+        {
+            return inch * 2.54;
+        }
+
+        public static double cmToinch(double cm)
+        {
+            return cm / 2.54;
+        }
+        
+    }
+}
---
+++ abgabe/L2HA_Keusch_Patrice/Aufgabe3/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace L2HA3_Keusch_Patrice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rndGen = new Random();
+            int[] array = new int[1000];
+            int zufallszahl;
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                zufallszahl = rndGen.Next(1, 2000);
+                if(Array.Exists(array, element => element == zufallszahl)){
+                     i--;
+                }
+                   
+                else
+                {
+                    array[i] = zufallszahl;
+                }
+                
+
+            }
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.WriteLine("Index: " + i + "Inhalt: " + array[i]);
+                
+            }
+            Console.Read();
+
+
+        }
+    }
+}
