e26888d0cc0caae1e4209d2fa3b6c7762e7627c8
Yzerman <yzerman.detroit@bluewin.ch>
2014-05-20 18:21:53 +0200

    .

---
+++ L10M3/Client/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var proxy = new ServiceReference1.Service1Client())
+            {
+                try
+                {
+                    Console.WriteLine(proxy.calcTwoInt(1, 2));
+
+                }
+                catch (TimeoutException timeout)
+                {
+                    proxy.Abort();
+                }
+                catch (CommunicationException comException)
+                {
+                    proxy.Abort();
+                }
+               
+            }
+            Console.ReadLine();
+        }
+    }
+}
