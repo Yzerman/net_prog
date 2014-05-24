55ff3fbd68ef94150c24bb69ab42683a92093844
Yzerman <yzerman.detroit@bluewin.ch>
2014-05-24 19:41:33 +0200

    .

---
+++ PivotApp1/PivotApp1/Properties/AssemblyInfo.cs
@@ -0,0 +1,37 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using System.Resources;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("PivotApp1")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("PivotApp1")]
+[assembly: AssemblyCopyright("Copyright ©  2014")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("e01d562d-7d30-4dd3-96a2-017139a4867d")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Revision and Build Numbers 
+// by using the '*' as shown below:
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: NeutralResourcesLanguageAttribute("en-US")]
---
+++ WcfPhoneAppTest1/PSRemoting_ConsoleProxy/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PSRemoting_ConsoleProxy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            using (var proxy = new ServiceReference1.Service1Client())
+            {
+                string ps_input = "hostname";
+
+                var item = proxy.malZwei(2);
+               // proxy.invokeCommandAsync(ps_input);
+                //string asd = proxy.invokeCommandAsync(ps_input);
+
+                Console.WriteLine(item);
+                Console.ReadLine();
+
+            }
+            
+
+        }
+    }
+}
